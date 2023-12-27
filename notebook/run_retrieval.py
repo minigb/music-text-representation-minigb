b061b8ed79f2e3b54656ec3d6eedc757781f437f
Minhee Lee <minigimbob@gmail.com>
2023-12-28 06:40:14 +0900

    snapshot

--- notebook/run_retrieval.py
+++ notebook/run_retrieval.py
@@ -26,6 +26,7 @@ msd_to_id = pickle.load(open(os.path.join(msd_path, "lastfm_annotation", "MSD_id
 id_to_path = pickle.load(open(os.path.join(msd_path, "lastfm_annotation", "7D_id_to_path.pkl"), 'rb'))
 annotation = json.load(open(os.path.join(msd_path, "ecals_annotation/annotation.json"), 'r'))
 
+
 def pre_extract_audio_embedding(framework, text_type, text_rep):
     ecals_test = torch.load(f"../mtr/{framework}/exp/transformer_cnn_cf_mel/{text_type}_{text_rep}/audio_embs.pt")
     msdid = [k for k in ecals_test.keys()]
@@ -33,40 +34,32 @@ def pre_extract_audio_embedding(framework, text_type, text_rep):
     audio_embs = torch.stack(audio_embs)
     return audio_embs, msdid
 
-def model_load(framework, text_type, text_rep):
+
+def retrieve(framework, text_type, text_rep, annotation, query_list):
+    # get audio embedding info
     audio_embs, msdid = pre_extract_audio_embedding(framework, text_type, text_rep)
-    model, tokenizer, config = get_model(framework=framework, text_type=text_type, text_rep=text_rep)
-    return model, audio_embs, tokenizer, msdid
+    # get pretrained model
+    model, tokenizer, _ = get_model(framework=framework, text_type=text_type, text_rep=text_rep)
 
-def retrieval_fn(query, tokenizer, model, audio_embs, msdid, annotation):
-    text_input = tokenizer(query, return_tensors="pt")['input_ids']
-    with torch.no_grad():
-        text_embs = model.encode_bert_text(text_input, None)
-    audio_embs = nn.functional.normalize(audio_embs, dim=1)
-    text_embs = nn.functional.normalize(text_embs, dim=1)
-    logits = text_embs @ audio_embs.T
-    ret_item = pd.Series(logits.squeeze(0).numpy(), index=msdid)
-    instance = {}
-    metadata = {}
-    for idx, _id in enumerate(ret_item.sort_values(ascending=False).head(3).index):
-        meta_obj = annotation[_id]
-        metadata[f'top{idx+1} music'] = meta_obj['tag']
-    return metadata
+    meta_results = []
+    for query in query_list:
+        text_input = tokenizer(query, return_tensors="pt")['input_ids']
+        with torch.no_grad():
+            text_embs = model.encode_bert_text(text_input, None)
+        
+        audio_embs = nn.functional.normalize(audio_embs, dim=1)
+        text_embs = nn.functional.normalize(text_embs, dim=1)
+        logits = text_embs @ audio_embs.T
+        ret_item = pd.Series(logits.squeeze(0).numpy(), index=msdid).sort_values(ascending=False)
+        
+        metadata = {}
+        for idx, _id in enumerate(ret_item.sort_values(ascending=False).head(3).index):
+            meta_obj = annotation[_id]
+            metadata[f'top{idx+1} music'] = meta_obj['tag']
 
-def retrieval_show(framework, text_type, text_rep, annotation, query, is_audio=False):    
-    model, audio_embs, tokenizer, msdid = model_load(framework, text_type, text_rep)
-    meta_results, retrieval_results = [], []
-    for i in query:
-        metadata = retrieval_fn(i, tokenizer, model, audio_embs, msdid, annotation)
-        # retrieval_results.append(instance)
         meta_results.append(metadata)
-    if is_audio:
-        inference = pd.DataFrame(retrieval_results, index=query)
-        html = inference.to_html(escape=False)
-    else:
-        inference = pd.DataFrame(meta_results, index=query)
-        html = inference.to_html(escape=False)
-    ipd.display(HTML(html))
+    return meta_results
+
 
 if __name__ == "__main__":
     tag_query = "banjo"
@@ -76,4 +69,4 @@ if __name__ == "__main__":
     text_type='bert' # tag, caption
     text_rep="stochastic"
 
-    retrieval_show(framework, text_type, text_rep, annotation, query, is_audio=False)
+    retrieve(framework, text_type, text_rep, annotation, query)

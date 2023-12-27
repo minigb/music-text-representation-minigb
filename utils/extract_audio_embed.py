2e0906fbfa44850c4e19768716d990013229b12f
Minhee Lee <minigimbob@gmail.com>
2023-12-28 06:46:43 +0900

    ss

---
+++ utils/extract_audio_embed.py
@@ -0,0 +1,27 @@
+# Modified the provided code in README
+import torch
+import numpy as np
+
+from mtr.utils.demo_utils import get_model
+from mtr.utils.audio_utils import load_audio, STR_CH_FIRST
+
+def audio_infer(audio_path, model, sr=16000, duration=None):
+    assert duration is not None, "audio duration must be specified"
+
+    audio, _ = load_audio(
+            path=audio_path,
+            ch_format= STR_CH_FIRST,
+            sample_rate= sr,
+            downmix_to_mono= True
+    )
+
+    input_size = int(duration * sr)
+    hop = int(len(audio) // input_size)
+    audio = np.stack([np.array(audio[i * input_size : (i + 1) * input_size]) for i in range(hop)]).astype('float32')
+    audio_tensor = torch.from_numpy(audio)
+
+    with torch.no_grad():
+        z_audio = model.encode_audio(audio_tensor)
+    audio_embs = z_audio.mean(0).detach().cpu()
+    
+    return audio_embs
